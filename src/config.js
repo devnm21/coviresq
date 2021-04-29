3f6fff03a5d819fddf0a1d3d35346cc59b645612
dev <pydev21@gmail.com>
2021-04-30 04:57:41 +0530

    add config file for Twit

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+	consumer_key: process.env.API_KEY,
+	consumer_secret: process.env.API_SECRET,
+	access_token: process.env.ACCESS_TOKEN,
+	access_token_secret: process.env.ACCESS_SECRET,
+}
